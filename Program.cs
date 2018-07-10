1e1ba0e448c83faecb1edb8382032dd5448b35bd
AndyLam <alam@neworbit.co.uk>
2018-07-10 16:10:39 +0100

    Initial code

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace HelloDelegate
+{
+    class Program
+    {
+        static Predicate<string> isUpperPred = s => s.All(char.IsUpper);
+        static Func<string, bool> isUpperFunc = s => s.All(char.IsUpper);
+
+        static bool IsUpperPred(Predicate<string> p, string input)
+        {
+            return p(input);
+        }
+
+        static void PrintIsUpper(string input)
+        {
+            Console.WriteLine($"{input} isUpper: {IsUpperPred(isUpperPred, input)}");
+            #if uncompilable
+            //Cannot pass a Func where a Predicate is expected
+            Console.WriteLine($"{input} isUpper: {IsUpperPred(isUpperFunc, input)}");
+            #endif
+        }
+        static void Main(string[] args)
+        {
+            PrintIsUpper("Hello");
+        }
+    }
+}
