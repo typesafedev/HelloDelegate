366791824244f6e7d098643e5dff56c626750774
AndyLam <alam@neworbit.co.uk>
2018-12-17 09:21:28 +0000

    Test EBCBot1

--- Program.cs
+++ Program.cs
@@ -8,34 +8,25 @@ namespace HelloDelegate
         static Predicate<string> isUpperPred = s => s.All(char.IsUpper);
         static Func<string, bool> isUpperFunc = s => s.All(char.IsUpper);
 
-        static bool IsUpperPred(Predicate<string> p, string input)
+        static bool Is(Predicate<string> p, string input)
         {
-            //Comment 2
-            //Comment 3
-            //Comment 4
-            //Comment 5
-            //Comment 6
-            //Comment 7 and 8
-            //Comment 9 and 10
-            //Comment 11 & 12
-            //Comment 13 & 14
-            //Comment 15 & 16
-            //Comment 17 & 18
             //Comment 19 & 20
             return p(input);
         }
 
         static void PrintIsUpper(string input)
         {
-            Console.WriteLine($"{input} isUpper: {IsUpperPred(isUpperPred, input)}");
+            Console.WriteLine($"{input} isUpper: {Is(isUpperPred, input)}");
             #if uncompilable
             //Cannot pass a Func where a Predicate is expected
-            Console.WriteLine($"{input} isUpper: {IsUpperPred(isUpperFunc, input)}");
+            Console.WriteLine($"{input} isUpper: {Is(isUpperFunc, input)}");
             #endif
         }
         static void Main(string[] args)
         {
             PrintIsUpper("Hello");
+            System.Diagnostics.Debugger.Break();
+            var id = Guid.NewGuid().ToString();
         }
     }
 }
